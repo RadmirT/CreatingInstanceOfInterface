e25ca18354dad93845c02b1dd2508ef21a51ab9b
radmirt <radmirt@mail.ru>
2023-09-08 09:49:01 +0500

    Init

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+
+IFoo obj = new IFoo();
+obj.SayHello();
+
+
+[ComImport, CoClass(typeof(Foo))]
+[Guid("00000000-0000-0000-0000-000123456789")]
+interface IFoo
+{
+    void SayHello();
+}
+
+class Foo : IFoo
+{
+    public void SayHello()
+    {
+        Console.WriteLine("Hello!");
+    }
+}
